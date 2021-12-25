cda291367848cac63319c20c267b7ad7d8f6a4ca
Slimey <56452910+Slimey2003@users.noreply.github.com>
2021-12-25 17:31:07 +0100

    Updated PonyAudioGuildController.java

--- src/main/java/io/schark/pony/core/feat/audio/PonyAudioGuildController.java
+++ src/main/java/io/schark/pony/core/feat/audio/PonyAudioGuildController.java
@@ -2,10 +2,13 @@ package io.schark.pony.core.feat.audio;
 
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayer;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import io.schark.pony.core.PonyManagerType;
 import io.schark.pony.core.feat.audio.handler.PonyAudioSendPlayerHandler;
 import io.schark.pony.exception.JoinVoiceFailedException;
 import lombok.Getter;
+import net.dv8tion.jda.api.audio.SpeakingMode;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.GuildVoiceState;
 import net.dv8tion.jda.api.entities.Member;
@@ -60,9 +63,80 @@ public class PonyAudioGuildController {
         this.guildManager.openAudioConnection(channel);
         this.player.setVolume(10);
         this.channel = channel;
-        this.timeout = new long[]{timeout};
+        this.timeout = new long[]{timeout, timeout};
         PonySearchQuarry quarry = new PonySearchQuarry(this.audioPlayerManager);
         consumer.accept(quarry);
+    }
+
+    public void moveBotToVoice(VoiceChannel channel) {
+        this.moveBotToVoice(channel, null);
+    }
+
+    public void moveBotToVoice(VoiceChannel channel, Consumer<PonySearchQuarry> consumer) {
+        if (channel == null) {
+            throw new JoinVoiceFailedException("Bot can´t Connected whit a Voice Channel because the Channel is null");
+        }
+        this.guildManager.openAudioConnection(channel);
+        this.channel = channel;
+        this.timeout[0] = timeout[1];
+        if (consumer != null) {
+            PonySearchQuarry quarry = new PonySearchQuarry(this.audioPlayerManager);
+            consumer.accept(quarry);
+        }
+    }
+
+
+
+    public boolean leaveVoice() {
+        if (this.channel == null) {
+            return false;
+        }
+        this.guildManager.closeAudioConnection();
+        return true;
+    }
+
+    public void setSpeakVolume(int volume) {
+        this.player.setVolume(volume);
+    }
+
+    public void setAutoReconnected(boolean reconnected) {
+        this.getGuildManager().setAutoReconnect(reconnected);
+    }
+
+    public void setNewTimeout(int timeout) {
+        this.timeout = new long[]{timeout, timeout};
+    }
+
+    public void resetTimeout() {
+        this.timeout[0] = this.timeout[1];
+    }
+
+    public void stopTrack() {
+        this.player.stopTrack();
+    }
+
+    public boolean pause() {
+        if (this.isPaused()) {
+            return false;
+        }
+        this.player.setPaused(true);
+        return true;
+    }
+
+    public boolean play() {
+        if (!this.isPaused()) {
+            return false;
+        }
+        this.player.setPaused(false);
+        return true;
+    }
+
+    public boolean isPaused() {
+        return this.player.isPaused();
+    }
+
+    public AudioTrack getPlayingTrack() {
+        return this.player.getPlayingTrack();
     }
 
     public void voiceTimeout() {
@@ -72,6 +146,7 @@ public class PonyAudioGuildController {
         new Thread(() -> {
             for (;this.timeout[0] > 0; this.timeout[0]--) {
                  if (this.channel.getMembers().size() > 1) {
+                     this.timeout[0] = this.timeout[1];
                      return;
                  }
             }
